2294884b5176662f8af2c14af9485a4caf48972c
Sriram Arvind L <lsriramarvind95@hotmail.com>
2019-11-24 18:41:05 +0530

    Adding Comments

--- 1/main.py
+++ 1/main.py
@@ -10,19 +10,22 @@ import requests
 """For commandline"""
 import sys
 
-#params or definitions
+"""Global Params Definition"""
 url="https://fourtytwowords.herokuapp.com"
 api_key="b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c112627073481104fec2fb1a0cc9d84c2212474c0cbe7d8e59d7b95c7cb32a1133f778abd1857bf934ba06647fda4f59e878d164"
 FAILED_CONSTANT = "requested_failed"
 
-#helper functions
+
 def response_data(raw_response_data): 
+    """Response Data Formation Function"""
     data = dict()
     data['status'] = raw_response_data.status_code
     data['data'] = raw_response_data.json()
     
     return data
+
 def requester(endpoint): 
+    """Resquest Data Function """
     f_url = url+endpoint
     raw_response_data = requests.get(url = f_url)
     f_data = response_data(raw_response_data)
@@ -30,6 +33,7 @@ def requester(endpoint):
 
 #api functions        
 def word_random(): 
+    """Request Random Word"""
     wrnd_endpoint="/words/randomWord?api_key={0}".format(api_key)
     data = requester(wrnd_endpoint)
     if(data['status'] == 200): 
@@ -37,7 +41,8 @@ def word_random():
     else:
         return FAILED_CONSTANT
     
-def word_definition(word): 
+def word_definition(word):
+    """Get Definition for a word """
     wdef_endpoint="/word/{0}/definitions?api_key={1}".format(word,api_key)
     data = requester(wdef_endpoint)
     
@@ -52,6 +57,7 @@ def word_definition(word):
     return definition
 
 def word_ant(word): 
+    """Get Antonmy for a word"""
     wrel_endpoint="/word/{0}/relatedWords?api_key={1}".format(word,api_key)
     data = requester(wrel_endpoint)
     
@@ -64,6 +70,7 @@ def word_ant(word):
         return list()           
 
 def word_syn(word): 
+    """Get Synonm for a word"""
     wrel_endpoint="/word/{0}/relatedWords?api_key={1}".format(word,api_key)
     data = requester(wrel_endpoint)
     
@@ -76,6 +83,7 @@ def word_syn(word):
         return list('')           
     
 def word_example(word): 
+    """Get Example for a word """
     weg_endpoint="/word/{0}/examples?api_key={1}".format(word,api_key)
     data = requester(weg_endpoint)
     example = list()
@@ -89,7 +97,7 @@ def word_example(word):
     return example    
 
 def word_full(word): 
-
+"""Display all Params of the given Word """
     print("Defn\n")
     for i in word_definition(word):
         print(i)
@@ -109,20 +117,24 @@ def word_full(word):
     
 
 def word_of_the_day(): 
+    """Display all params of a random word """
     word = word_random()
     
     if(word != FAILED_CONSTANT): 
+        print("Word of the day {}".format(word))
         word_full(word)
     
     else: 
         print("No Word for the day")
 
 def shuffleWord(word): 
+    """Shuffle a Given word in any order """
     tempWord = list(word)
     random.shuffle(tempWord)
     return ''.join(tempWord)
     
 def validateAnswer(word,syn):
+    """Check function for the final answer """
     answer = str(input("The Word is?:")).lower()
     if(answer == word or answer in syn): 
         return True
@@ -130,6 +142,7 @@ def validateAnswer(word,syn):
 
 def showHints(word,definitions,ant,syn,comingBack=False): 
 #state of variable has to be maintained,only for word play
+    """ Display Hint for the Play Function"""
     if(comingBack): 
         print("Jumbled Form of Word:{}".format(shuffleWord(word)))
         
@@ -137,7 +150,7 @@ def showHints(word,definitions,ant,syn,comingBack=False):
         randomChoice = random.randint(0,len(definitions) - 1)
         print("Definition:Clue")
         print(definitions[randomChoice])
-        del definitions[randomChoice]
+        
     
     if(len(syn) > 0 and len(ant) > 0):
         choice = random.randint(0,1)
@@ -146,28 +159,29 @@ def showHints(word,definitions,ant,syn,comingBack=False):
             print("Syn:Clue")
             randomChoice = random.randint(0,len(syn) -1) 
             print(syn[randomChoice])
-            del syn[randomChoice]
+            
 
         if(choice == 1):
             print("Ant:Clue")
             randomChoice = random.randint(0,len(ant) - 1) 
             print(ant[randomChoice])
-            del ant[randomChoice]
+            
 
     
     elif(len(syn) > 0 and len(ant) == 0):
         print("Syn:Clue")
         randomChoice = random.randint(0,len(syn) - 1) 
         print(syn[randomChoice])
-        del syn[randomChoice]
+        
         
     elif(len(ant) > 0 and len(syn) == 0):
         print("Ant:Clue")
         randomChoice = random.randint(0,len(syn) - 1) 
         print(ant[randomChoice])
-        del randomChoice
+        
     
 def word_play(): 
+    """Play Function"""
     word = word_random()
     definitions = word_definition(word)
     ant = word_ant(word)
@@ -198,6 +212,7 @@ def word_play():
             continue
     
 def main():
+    """Command Line Function"""
     if(len(sys.argv) > 3):
         print("Execute with one params and one arg,for eg\n1- defn <word>\n2- syn<word>\n3- ant<word>\n4- ex<word>\n5- <word>\n6- just execute\n7- play")
     
@@ -224,25 +239,15 @@ def main():
             print("Examples\n")
             for i in word_example(word):
                 print(i)
-    
+        
     if(len(sys.argv) == 2):
         param = sys.argv[1]
         if(param == 'play'):
             word_play()
-        elif(param == '/^[a-zA-Z]+$/'):
-            word_full(word)
+        else:
+            word_full(param)
             
     if(len(sys.argv) == 1):
         word_of_the_day()
-        
-main()      
-    
-    
 
-
-
-
-
-
-
-
+main()
